203f1790497ce1b488e0de1e0f0f792640111022
Adit Syed Afnan <111628226+S-A-Adit@users.noreply.github.com>
2025-09-16 18:57:09 -0400

    Delete clubhouse directory

--- clubhouse/src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-export const ensureAuth = (req,res,next) =>{
-    if (req.isAuthenticated()) return next();
-  res.redirect('/login');
-};
-
-export const ensureAdmin = (req,res,next) =>{
-  if (req.isAuthenticated() && req.user.is_admin) return next();
-  res.status(403).send('Forbidden');
-}
